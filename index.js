44a4c872c7e18d9f3dab424d953801f2bb0f40d6
Rahulkannoujia123 <aryankannoujia@gmail.com>
2025-01-20 15:40:25 +0530

    vps port update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const userRoutes=require('./src/route/shfe')
 // Importing Metal model
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.use('/api', userRoutes);
 // MongoDB connection
 mongoose.connect('mongodb+srv://Rahul:myuser@rahul.fack9.mongodb.net/SHFESCRAP?authSource=admin&replicaSet=atlas-117kuv-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true', {
